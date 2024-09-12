17deaccd431ca0e34cee5cacf6d3e8d8b7d8634e
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2024-09-12 21:38:59 +0100

    add charts

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+const UserCard = ({ type }: { type: string }) => {
+  return (
+    <div className="rounded-2xl odd:bg-adaPurple even:bg-adaYellow flex-1 p-4 min-w-[130px]">
+      <div>
+        <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">
+          2023/2024
+        </span>
+        <Image src="/more.png" alt="" width={20} height={20} />
+      </div>
+      <h1 className="text-2xl font-semibold my-4">3,455</h1>
+      <h2 className="capitalize text-sm text-gray-500">{type}</h2>
+    </div>
+  );
+};
+
+export default UserCard;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        adaSky: "#C3EBFA",
+        adaSkyLight: "#EDF9FD",
+        adaPurple: "#CFCEFF",
+        adaPurpleLight: "#F1F0FF",
+        adaYellow: "#FAE27C",
+        adaYellowLight: "#FEFCE8",
+      },
     },
   },
   plugins: [],
