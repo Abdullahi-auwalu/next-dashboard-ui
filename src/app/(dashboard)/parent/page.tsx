318afab95afd9801e6ba470e25f0a0d5aff9444e
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2024-10-01 21:39:13 +0100

    create student, teacher and parent page

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,5 +1,23 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
 const ParentPage = () => {
-  return <div>ParentPage</div>;
+  return (
+    <div className="p-4 flex flex-1 gap-4 flex-col lg:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (Abdullahi Auwalu)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default ParentPage;
